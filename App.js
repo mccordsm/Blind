de898f8d2b5fb4c56f7c7e8ae7cc2c4771307939
smccord <sean.mccord@wsu.edu>
2022-12-09 02:54:55 -0800

    added emegency and navigation changes

--- App.js
+++ App.js
@@ -20,6 +20,14 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     alignItems: 'center',
   },
+  destView: {
+    backgroundColor: '#FF0F56',
+    borderRadius: 30,
+    width: '50%',
+    height: 45,
+    marginBottom: 5,
+    alignItems: 'center',
+  },
   TextInput: {
     flex: 1,
     padding: 10,
@@ -40,18 +48,31 @@ const styles = StyleSheet.create({
   },
   title: {
     fontWeight: 'bold',
+    fontSize: 50,
+    alignItems: "center",
+    paddingBottom: 100,
+  },
+  subTitle: {
+    fontWeight: 'bold',
     fontSize: 20,
     alignItems: "center",
-    paddingBottom: 30,
+    paddingBottom: 10,
   },
   loginText: {
     marginBottom: 20,
     padding: 30
   },
+  pushText: {
+    marginBottom: 20,
+    padding: 30,
+  },
+  emergText: {
+    marginBottom: 50,
+  },
   space: {
     width: 20, // or whatever size you need
     height: 20,
-  }
+  },
 });
 
 function LoginScreen({navigation}) {
@@ -79,18 +100,39 @@ function LoginScreen({navigation}) {
           />
         </View> 
         <Button 
+          style={styles.pushText}
+          title="Push To Talk"
+        />
+        <View style={styles.space} />
+        <Button 
         style={styles.loginText}
         title="LOGIN"
         onPress={() => navigation.navigate('Home')}
+        />
+        <View style={styles.space} />
+        <Button 
+        style={styles.loginText}
+        title="Create Account"
+        onPress={() => navigation.navigate('Create')}
         />
     </View>
   );
 }
 
 function HomeScreen({navigation}) {
+  const [Destination, setDestination] = useState('');
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <Button 
+      style={styles.emergText}
+      title="Emergency Services"
+      onPress={() => navigation.navigate('Emergency')}
+      />
+      <View style={styles.space} />
+      <View style={styles.space} />
+      <View style={styles.space} />
+      <View style={styles.space} />
       <Text style={styles.title}>THIEA</Text>
 
       <View style={styles.inputView}>
@@ -98,13 +140,12 @@ function HomeScreen({navigation}) {
           style={styles.TextInput}
           placeholder="Current Destination"
           placeholderTextColor="#003f5c"
-          onChangeText ={(user) => setUser(user)}
+          onChangeText ={(Destination) => setDestination(Destination)}
         />
       </View>
       <Button 
       style={styles.loginText}
       title="Push To Talk"
-      onPress={() => navigation.navigate('Final')}
       />
       <View style={styles.space} />
       <Button 
@@ -112,11 +153,112 @@ function HomeScreen({navigation}) {
       title="Library"
       onPress={() => navigation.navigate('Library')}
       />
+      <View style={styles.space} />
+      <Button 
+      style={styles.loginText}
+      title="Begin Navigation"
+      />
+      <View style={styles.space} />
+      <Button 
+      style={styles.loginText}
+      title="End Navigation"
+      onPress={() => navigation.navigate('Final')}
+      />
+      <View style={styles.space} />
+      <Button 
+      style={styles.loginText}
+      title="Log Out"
+      onPress={() => navigation.navigate('Login')}
+      />
     </View>
   );
 }
 
 function LibraryScreen({navigation}) {
+  const [Favorite1, setFavorite1] = useState('');
+  const [Favorite2, setFavorite2] = useState('');
+  const [Favorite3, setFavorite3] = useState('');
+
+  const [Recent1, setRecent1] = useState('');
+  const [Recent2, setRecent2] = useState('');
+  const [Recent3, setRecent3] = useState('');
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Button 
+      style={styles.emergText}
+      title="Emergency Services"
+      onPress={() => navigation.navigate('Emergency')}
+      />
+      <View style={styles.space} />
+      <Text style={styles.title}>THIEA</Text>
+      <Text style={styles.subTitle}>Recent Destinations</Text>
+      <View style={styles.destView}>
+        <TextInput 
+          style={styles.TextInput}
+          placeholder="Recent Destination"
+          placeholderTextColor="#003f5c"
+          onChangeText ={(Recent1) => setRecent1(Recent1)}
+        />
+      </View>
+      <View style={styles.destView}>
+        <TextInput 
+          style={styles.TextInput}
+          placeholder="Recent Destination"
+          placeholderTextColor="#003f5c"
+          onChangeText ={(Recent2) => setRecent2(Recent2)}
+        />
+      </View>
+      <View style={styles.destView}>
+        <TextInput 
+          style={styles.TextInput}
+          placeholder="Recent Destination"
+          placeholderTextColor="#003f5c"
+          onChangeText ={(Recent3) => setRecent3(Recent3)}
+        />
+      </View>
+      <View style={styles.space} />
+      <Text style={styles.subTitle}>Favorite Destinations</Text>
+      <View style={styles.destView}>
+        <TextInput 
+          style={styles.TextInput}
+          placeholder="Favorite Destination"
+          placeholderTextColor="#003f5c"
+          onChangeText ={(Favorite1) => setFavorite1(Favorite1)}
+        />
+      </View>
+      <View style={styles.destView}>
+        <TextInput 
+          style={styles.TextInput}
+          placeholder="Favorite Destination"
+          placeholderTextColor="#003f5c"
+          onChangeText ={(Favorite2) => setFavorite2(Favorite2)}
+        />
+      </View>
+      <View style={styles.destView}>
+        <TextInput 
+          style={styles.TextInput}
+          placeholder="Favorite Destination"
+          placeholderTextColor="#003f5c"
+          onChangeText ={(Favorite3) => setFavorite3(Favorite3)}
+        />
+      </View>
+      <View style={styles.space} />
+      <Button 
+      style={styles.loginText}
+      title="Push To Talk"
+      />
+      <View style={styles.space} />
+      <Button 
+      style={styles.loginText}
+      title="Home"
+      onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+function CreateScreen({navigation}) {
 
   return (
     <View style={styles.container}>
@@ -126,7 +268,7 @@ function LibraryScreen({navigation}) {
       <View style={styles.inputView}>
         <TextInput 
           style={styles.TextInput}
-          placeholder="Recent Destination"
+          placeholder="User Name"
           placeholderTextColor="#003f5c"
           onChangeText ={(user) => setUser(user)}
         />
@@ -134,7 +276,7 @@ function LibraryScreen({navigation}) {
       <View style={styles.inputView}>
         <TextInput 
           style={styles.TextInput}
-          placeholder="Recent Destination"
+          placeholder="Password"
           placeholderTextColor="#003f5c"
           onChangeText ={(user) => setUser(user)}
         />
@@ -142,7 +284,7 @@ function LibraryScreen({navigation}) {
       <View style={styles.inputView}>
         <TextInput 
           style={styles.TextInput}
-          placeholder="Recent Destination"
+          placeholder="Address"
           placeholderTextColor="#003f5c"
           onChangeText ={(user) => setUser(user)}
         />
@@ -150,7 +292,7 @@ function LibraryScreen({navigation}) {
       <View style={styles.inputView}>
         <TextInput 
           style={styles.TextInput}
-          placeholder="Previous Destination"
+          placeholder="Phone Number"
           placeholderTextColor="#003f5c"
           onChangeText ={(user) => setUser(user)}
         />
@@ -158,15 +300,7 @@ function LibraryScreen({navigation}) {
       <View style={styles.inputView}>
         <TextInput 
           style={styles.TextInput}
-          placeholder="Previous Destination"
-          placeholderTextColor="#003f5c"
-          onChangeText ={(user) => setUser(user)}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput 
-          style={styles.TextInput}
-          placeholder="Previous Destination"
+          placeholder="ZIP"
           placeholderTextColor="#003f5c"
           onChangeText ={(user) => setUser(user)}
         />
@@ -174,34 +308,83 @@ function LibraryScreen({navigation}) {
       <Button 
       style={styles.loginText}
       title="Push To Talk"
-      onPress={() => navigation.navigate('Home')}
       />
       <View style={styles.space} />
       <Button 
       style={styles.loginText}
-      title="Library"
-      onPress={() => navigation.navigate('Library')}
+      title="Login"
+      onPress={() => navigation.navigate('Home')}
       />
     </View>
-    
   );
 }
 
 function FinalScreen({navigation}) {
   return (
     <View style={{ flex: 1,backgroundColor:"#fff", alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{fontSize: 20, fontWeight: "bold",}}>Destination Reached</Text>
+      <Button 
+      style={styles.emergText}
+      title="Emergency Services"
+      onPress={() => navigation.navigate('Emergency')}
+      />
+      <View style={styles.space} />
+      <View style={styles.space} />
+      <View style={styles.space} />
+      <View style={styles.space} />
+      <Text style={{fontSize: 20, fontWeight: "bold",}}>Destination Reached/Ended</Text>
       <View style={styles.space} />
       <Button 
       style={styles.loginText}
       title="Push To Talk"
-      onPress={() => navigation.navigate('Home')}
       />
       <View style={styles.space} />
       <Button 
       style={styles.loginText}
       title="Library"
       onPress={() => navigation.navigate('Library')}
+      />
+      <View style={styles.space} />
+      <Button 
+      style={styles.loginText}
+      title="Return Home"
+      onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+function EmergencyScreen({navigation}) {
+  return (
+    <View style={{ flex: 1,backgroundColor:"#fff", alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.space} />
+      <View style={styles.space} />
+      <View style={styles.space} />
+      <View style={styles.space} />
+      <Text style={{fontSize: 30, fontWeight: "bold",}}>Fall Detected/Location Lost!!</Text>
+      <View style={styles.space} />
+      <Text style={{fontSize: 20, fontWeight: "bold",}}>Calling in 10 Seconds</Text>
+      <Text style={{fontSize: 20, fontWeight: "bold",}}>(425)123-5555</Text>
+      <View style={styles.space} />
+      <Button 
+      style={styles.loginText}
+      title="Push To Talk"
+      />
+      <View style={styles.space} />
+      <Button 
+      style={styles.loginText}
+      title="Stop Emergency"
+      />
+      <View style={styles.space} />
+      <Button 
+      style={styles.loginText}
+      title="Library"
+      onPress={() => navigation.navigate('Library')}
+      />
+      <View style={styles.space} />
+      <Button 
+      style={styles.loginText}
+      title="Return Home"
+      onPress={() => navigation.navigate('Home')}
       />
     </View>
   );
@@ -217,6 +400,8 @@ function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Final" component={FinalScreen} />
         <Stack.Screen name="Library" component={LibraryScreen} />
+        <Stack.Screen name="Create" component={CreateScreen} />
+        <Stack.Screen name="Emergency" component={EmergencyScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
